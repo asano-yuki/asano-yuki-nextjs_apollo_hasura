f2ebed99f1b0b517764f557a30ccad993ab5abb5
asano-yuki <asanoyuuki0523@gmail.com>
2022-10-11 22:09:32 +0900

    fix: omission of adding commits

---
+++ lib/apolloClient.ts
@@ -0,0 +1,27 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  NormalizedCacheObject
+} from '@apollo/client'
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+export const createApolloClient = () => new ApolloClient({
+  ssrMode: typeof window === 'undefined',
+  link: new HttpLink({
+    uri: "https://diverse-chow-57.hasura.app/v1/graphql"
+  }),
+  cache: new InMemoryCache(),
+});
+
+export const initializeApollo = (initialState = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient()
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}
---
+++ pages/local-state-b.tsx
@@ -0,0 +1,13 @@
+import type { VFC } from 'react'
+import { LocalStateB } from '../components/LocalStateB'
+import { Layout } from '../components/Layout'
+
+const LocalStatePageB: VFC = () => {
+  return (
+    <Layout title="Local state B">
+      <LocalStateB />
+    </Layout>
+  )
+}
+
+export default LocalStatePageB
